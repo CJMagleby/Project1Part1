95d6fe8b0c4333f0d274b391f2057b7afdf056e1
CJ Magleby <cjmagleby@gmail.com>
2018-10-03 20:48:11 -0700

    First part of project 1

--- Project1Part1/main.cpp
+++ Project1Part1/main.cpp
@@ -1,15 +1,70 @@
-//
-//  main.cpp
-//  Project1Part1
-//
-//  Created by CJ Magleby on 10/3/18.
-//  Copyright © 2018 CJ Magleby. All rights reserved.
-//
-
 #include <iostream>
+#include <string>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+class Animal
+{
+protected:
+    std::string name;
+    int age;
+public:
+    Animal(std::string passName, int passAge)
+    {
+        name = passName;
+        age = passAge;
+    }
+    Animal()
+    {
+        name = "NULL";
+        age = 0;
+    }
+    std::string getName() { return name; }
+    void setName(std::string newName) { name = newName; }
+    int getAge() { return age; }
+    void setAge(int newAge) { age = newAge; }
+    virtual void print() = 0;
+};
+
+class Zebra : public Animal
+{
+protected:
+    std::string location;
+public:
+    Zebra(std::string passName, int passAge) : Animal(passName, passAge) { location = "Africa"; }
+    Zebra() : Animal() { location = "Africa"; }
+    std::string getLocation() { return location; }
+    void print()
+    {
+        std::cout << "Name: " << name << " Age: " << age << " Location: " << location << std::endl;
+    }
+};
+
+class Dolphin : public Animal
+{
+protected:
+    std::string location;
+public:
+    Dolphin(std::string passName, int passAge) : Animal(passName, passAge) { location = "Ocean"; }
+    Dolphin() : Animal() { location = "Ocean"; }
+    std::string getLocation() { return location; }
+    void print()
+    {
+        std::cout << "Name: " << name << " Age: " << age << " Location: " << location << std::endl;
+    }
+};
+
+int main()
+{
+    std::string dolphinName = "Flipper";
+    int dolphinAge = 10;
+    Dolphin flipper(dolphinName, dolphinAge);
+    flipper.print();
+    
+    std::string zebraName = "Marty";
+    int zebraAge = 4;
+    Zebra marty(zebraName, zebraAge);
+    marty.print();
+    
+    
+    system("Pause");
     return 0;
 }
